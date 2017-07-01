957b30148080b3daeb51e580b9c5cb89400eafb3
Devkeep <363927173@qq.com>
2017-07-02 01:10:39 +0800

    Loader.php
    Loader 缓存

--- Loader.php
+++ Loader.php
@@ -8,18 +8,18 @@
 
 require(__DIR__ . '/PHPCache.php');
 
-$Cache = PHPCache::Loader('cache.json');
+$conf = 'cache.json';
 
 //set()方法 参数：key, value, time  true成功，false失败
-$Cache->set('page', 2, 20);					
+PHPCache::Loader($conf)->set('page', 2, 20);					
 
 //get()方法，参数：key  true成功，false失败
-$result = $Cache->get('page');
+PHPCache::Loader($conf)->get('page');
 
 //delete()方法，参数：key  true成功，false失败
-$Cache->delete('page');
+PHPCache::Loader($conf)->delete('page');
 
 //clear()方法， 请空缓存
-$result = $Cache->clear();
+$result = PHPCache::Loader($conf)->clear();
 
 //日志文件 Cache.log
